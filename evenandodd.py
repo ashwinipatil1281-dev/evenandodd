1013ca6dd50affa63e3d14d6857fb1bf53caa080
ashwinipatil1281-dev <ashwinipatil1281@gmail.com>
2025-12-11 20:31:10 +0530

    Update evenandodd.py

--- evenandodd.py
+++ evenandodd.py
@@ -1,19 +1,21 @@
 import sys
 
-if len(sys.argv) < 2:
-    print("Usage: python even_odd.py <num1> <num2> <num3> ...")
-    sys.exit(1)
+# Default values if user does not provide input
+default_numbers = [5, 10, 15]
 
-numbers = list(map(int, sys.argv[1:]))
+# If user provides numbers, use them; otherwise use defaults
+if len(sys.argv) > 1:
+    numbers = list(map(int, sys.argv[1:]))
+    source = "User Input"
+else:
+    numbers = default_numbers
+    source = "Self-assigned"
 
-even_count = 0
-odd_count = 0
+print(f"{source} Numbers: {numbers}")
 
-for n in numbers:
-    if n % 2 == 0:
-        even_count += 1
-    else:
-        odd_count += 1
+# Count evens and odds
+even_count = sum(1 for n in numbers if n % 2 == 0)
+odd_count = sum(1 for n in numbers if n % 2 != 0)
 
 print("Count of Even Numbers:", even_count)
 print("Count of Odd Numbers:", odd_count)
